0c0bad873cebaba399b308d4c95e49bbf03dc33f
Ekaterina Ryzhova <rubicante@icloud.com>
2019-09-04 11:46:48 +0300

    Initial Commit

---
+++ FlexiblePageControlDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlexiblePageControlDemo
+//
+//  Created by Ekaterina Ryzhova on 04/09/2019.
+//  Copyright © 2019 Ekaterina Ryzhova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
